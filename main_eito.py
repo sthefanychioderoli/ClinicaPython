b6bd5d600895dc1aec4f0a31a243a7b165f09be8
Heitor Prado <heitorpradowow@gmail.com>
2024-06-06 10:44:23 -0300

    alteração calc soro e medicamento

--- main_eito.py
+++ main_eito.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+def calcular_volume(desidratacao, peso):
+    if desidratacao == 'leve':
+        return 50 * peso
+    elif desidratacao == 'moderada':
+        return 75 * peso
+    elif desidratacao == 'grave':
+        return 100 * peso
+    else:
+        return 0
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    volume = None
+    dose = None
+    if request.method == 'POST':
+        if 'peso_fluidoterapia' in request.form:
+            grau_desidratacao = request.form['desidratacao']
+            peso = float(request.form['peso_fluidoterapia'])
+            volume = calcular_volume(grau_desidratacao, peso)
+        if 'peso_medicamento' in request.form:
+            peso = float(request.form['peso_medicamento'])
+            dose_recomendada = float(request.form['dose_recomendada'])
+            dose = peso * dose_recomendada
+    return render_template('calc_medicamento_calc_soro.html', volume=volume, dose=dose)
 
-@app.route('')
-
-
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
